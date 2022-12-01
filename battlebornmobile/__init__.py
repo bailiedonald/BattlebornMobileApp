fc3d8477779552b959d9a7e630f78fc5d8ae2078
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2022-11-30 23:32:07 -0800

    gd

--- battlebornmobile/__init__.py
+++ battlebornmobile/__init__.py
@@ -5,7 +5,6 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = 'Super Secret Password'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///battleborn.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost:5432/battleborn'
 
 db = SQLAlchemy(app)
