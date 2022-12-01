0fc4894f72b008a79a72436eab987fbac6f337cd
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2022-11-30 23:49:02 -0800

    load

--- battlebornmobile/__init__.py
+++ battlebornmobile/__init__.py
@@ -3,10 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)
+
 app.config['SECRET_KEY'] = 'Super Secret Password'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost:5432/battleborn'
 
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from battlebornmobile import views
