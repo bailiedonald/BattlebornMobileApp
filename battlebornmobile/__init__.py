8f05b629a6e7cfe62a67b4d8fc49b7e769b38f29
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2022-11-30 23:05:51 -0800

    gg

--- battlebornmobile/__init__.py
+++ battlebornmobile/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Super Secret Password'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///battleborn'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///battleborn.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost:5432/battleborn'
 
 db = SQLAlchemy(app)
