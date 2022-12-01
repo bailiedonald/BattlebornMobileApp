6183663e53fcd749b0e011ccf3beb7872af5a917
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2022-11-30 23:08:46 -0800

    postgres works

--- battlebornmobile/__init__.py
+++ battlebornmobile/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Super Secret Password'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///battleborn.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost:5432/battleborn'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///battleborn.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost:5432/battleborn'
 
 db = SQLAlchemy(app)
 
