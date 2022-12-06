69757a10ec5c62568dfb814017634bbd8eb6bea1
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2022-12-05 21:27:26 -0800

    fixed

--- battlebornmobile/forms.py
+++ battlebornmobile/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from battlebornmobile.models import User, Staff, Customer,Pet
+from battlebornmobile.models import User, StaffLogin, CustomerLogin,Pet
 
 class SignUpForm(FlaskForm):
     username = StringField('Username',
@@ -12,12 +12,12 @@ class SignUpForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        user = Customer.query.filter_by(username=username.data).first()
+        user = CustomerLogin.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        user = Customer.query.filter_by(email=email.data).first()
+        user = CustomerLogin.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
 
@@ -44,11 +44,11 @@ class StaffSignUpForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        user = Staff.query.filter_by(username=username.data).first()
+        user = StaffLogin.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
+        user = StaffLogin.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
