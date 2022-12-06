8b10979263e9c24c54d85b9dde5251b9c6c8ed2f
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2022-12-06 11:37:48 -0800

    ok cool

--- migrate.py
+++ migrate.py
@@ -1,7 +1,7 @@
 # shell db script to migrate
 # usage: Python3 migrate.py
 from battlebornmobile import app, db
-from battlebornmobile.models import User, Staff, Customer, Pet
+from battlebornmobile.models import User, Pet, Appointment
 app.app_context().push()
 db.create_all()
 exit()
