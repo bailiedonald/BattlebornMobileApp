9c446bdbacf02444da071cba05c2dfd87f45db63
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2022-12-05 20:00:53 -0800

    db fix

--- battlebornmobile/models.py
+++ battlebornmobile/models.py
@@ -7,7 +7,7 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-'''
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -18,7 +18,6 @@ class User(db.Model, UserMixin):
     
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-'''
 
 class Customer(db.Model, UserMixin):
     CustomerID = db.Column(db.Integer, primary_key=True)
