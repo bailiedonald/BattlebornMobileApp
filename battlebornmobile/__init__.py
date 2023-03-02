496711a569a97a2a7cadd6af180c92d18917533f
ddesarle <davisdesarle@gmail.com>
2023-03-02 15:35:33 -0800

    SMS Appointment Notification
    Ready for testing.

--- battlebornmobile/__init__.py
+++ battlebornmobile/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, roles_required
+from twilio.rest import Client
+import os
+
 
 
 
@@ -20,5 +23,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+# Initialize the Twilio client with account SID and auth token 
+account_sid = os.environ['AC003773b4742a681273555f869fe8c6d1']
+auth_token = os.environ['TWILIO6406c4546b69b0c05ddd4904ca160eb4_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
 
 from battlebornmobile import views
