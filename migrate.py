88b885dedfa3ddfa39fcb07628253a277b8afc0d
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2023-04-01 16:15:08 -0700

    Revert "l"
    This reverts commit 89b6e97029feae1f182b3d90dde66856f6c2f3e8.

--- migrate.py
+++ migrate.py
@@ -1,13 +1,7 @@
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+# shell db script to migrate
+# usage: Python3 migrate.py
 from battlebornmobile import app, db
 from battlebornmobile.models import User, Pet, Appointment
-
-app.config.from_object('config.DevelopmentConfig')
-migrate = Migrate(app, db)
-
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
-
-if __name__ == '__main__':
-    manager.run()
+app.app_context().push()
+db.create_all()
+exit()
