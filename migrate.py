89b6e97029feae1f182b3d90dde66856f6c2f3e8
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2023-04-01 15:27:04 -0700

    l

--- migrate.py
+++ migrate.py
@@ -1,7 +1,13 @@
-# shell db script to migrate
-# usage: Python3 migrate.py
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from battlebornmobile import app, db
 from battlebornmobile.models import User, Pet, Appointment
-app.app_context().push()
-db.create_all()
-exit()
+
+app.config.from_object('config.DevelopmentConfig')
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
