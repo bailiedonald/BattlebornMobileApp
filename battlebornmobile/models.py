a207559713c8f7a1235f2ccc0bef3de1892f2e82
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2023-04-13 12:34:49 -0700

    comments

--- battlebornmobile/models.py
+++ battlebornmobile/models.py
@@ -10,7 +10,7 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-#User Database Table
+#User Database
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
