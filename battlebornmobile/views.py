e53a72bf961af7e543110619a27bfbbfcfac4826
Donald E Bailie <52550390+bailiedonald@users.noreply.github.com>
2023-04-25 20:33:30 -0700

    final fixes

--- battlebornmobile/views.py
+++ battlebornmobile/views.py
@@ -221,7 +221,7 @@ def profile_update():
         if staff == True:
             return render_template("dashboardstaff.html")
         else:
-            flash ("Access Denied Staff Only.")
+            flash ("Your New Photo Is Ready")
             return render_template("dashboard.html")
     elif request.method == 'GET':
         form.username.data = current_user.username
@@ -257,9 +257,9 @@ def profile_picture_update():
 
         flash('Your profile picture has been updated!', 'success')
         if staff == True:
-            return render_template("dashboardstaff.html")
+            return redirect(url_for('index'))
         else:
-            flash ("Access Denied Staff Only.")
+            flash ("Your Account Has Been Updated.")
             return render_template("dashboard.html")
     return render_template('profile_picture_update.html', form=form)
 
